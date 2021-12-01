111bfa1240f78239baee4066db33dca306cd2c9b
8iondo <44554381+8iondo@users.noreply.github.com>
2021-12-01 18:57:28 +0100

    Added Min and Max temperature
    Removed uneeded "Loading..." string

--- apps/thermom/app.js
+++ apps/thermom/app.js
@@ -1,11 +1,17 @@
+var min = 9999;
+var max = 0;
+
 function onTemperature(p) {
   g.reset(1).clearRect(0,24,g.getWidth(),g.getHeight());
   g.setFont("6x8",2).setFontAlign(0,0);
   var x = g.getWidth()/2;
   var y = g.getHeight()/2 + 10;
-  g.drawString("Temperature:", x, y - 45);
+  max = Math.max(max, p.temperature.toFixed(1)) ;
+  min = Math.min(min, p.temperature.toFixed(1)) ;
+
+  g.drawString("Min " + min + " Max " + max, x, y - 45);
   g.setFontVector(70).setFontAlign(0,0);
-  g.drawString(p.temperature.toFixed(1) + " °C", x, y);
+  g.drawString(p.temperature.toFixed(1), x, y);
 }
 
 function drawTemperature() {
@@ -18,11 +24,10 @@ function drawTemperature() {
   }
 }
 
-
 setInterval(function() {
   drawTemperature();
 }, 20000);
 drawTemperature();
-E.showMessage("Reading temperature...");
+E.showMessage("Loading...");
 Bangle.loadWidgets();
 Bangle.drawWidgets();
