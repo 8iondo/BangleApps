7272fb6daa36c1dfbb9a207be43cfd381d2d915b
Lou Beyond <8iondo@gamail.com>
2021-12-09 19:49:20 +0100

    thermom guides for grph

--- apps/thermom/app.js
+++ apps/thermom/app.js
@@ -9,11 +9,14 @@ const conf = {
   }
 };
 
-function onTemperature(p) {
-  const temp = isNaN(p.temperature.toFixed(1)) ? Math.floor(Math.random() * 10) - 5: p.temperature.toFixed(1);
-  console.log(temp);
-  scans.push(temp);
+const grHeight = 120;
+const grBottom = g.getHeight();
+const grTop = grBottom - grHeight;
+const grWidth = g.getWidth();
 
+function onTemperature(p) {
+  const temp = isNaN(p.temperature.toFixed(1)) ? Math.floor(Math.random() * 6) + 15 : p.temperature.toFixed(1);
+  scans.push(temp);
   max = Math.max(max, temp);
   min = Math.min(min, temp);
 
@@ -26,13 +29,6 @@ function onTemperature(p) {
   g.drawString(temp, x, y);
   g.setFontVector(15).setFontAlign(1, 1, 1);
 
-  const grHeight = 120;
-  const grBottom = g.getHeight();
-  const grTop = grBottom - grHeight;
-  const grWidth = g.getWidth();
-  const offset = (max - min < 2) ? 2 : 0.2;
-  const grMin = min - offset;
-  const grMax = (max - min < 1) ? max + offset : max;
   const itemwidth = Math.max(5, Math.min(15, grWidth / scans.length));
   if (itemwidth == 5) {
     // max array size
@@ -40,11 +36,14 @@ function onTemperature(p) {
   }
 
   g.reset(1).clearRect(0, grTop, grWidth, grBottom);
+  
+  g.setColor(0.36, 0.36, 0.36);
+  g.setFont("6x8", 1);
+  drawGuides();
+
+  g.setColor(255, 255, 255);
   scans.forEach((t, index) => {
-    valRatio = (t - grMin) / (grMax - grMin);
-
-    pos = grBottom - Math.floor(valRatio * grHeight);
-
+    pos = fromTempToY(t);
     g.fillRect(index * itemwidth, pos, (index * itemwidth) + itemwidth - 2, grBottom);
     // g.drawString(t, index * 20, pos );
   });
@@ -58,6 +57,16 @@ function onTemperature(p) {
 
 }
 
+function drawGuides() {
+  var guideTemp = Math.floor(min + 1);
+  while (guideTemp < max) {
+    yline = fromTempToY(guideTemp);
+    g.fillRect(1, yline, grWidth, yline + 1);
+    g.drawString(guideTemp, grWidth - 20, yline - 9);
+    guideTemp++;
+  }
+}
+
 function drawTemperature() {
   if (Bangle.getPressure) {
     Bangle.getPressure().then(onTemperature);
@@ -66,6 +75,14 @@ function drawTemperature() {
       temperature: E.getTemperature()
     });
   }
+}
+
+function fromTempToY(t) {
+  const offset = (max - min < 2) ? 2 : 0.2;
+  const grMin = min - offset;
+  const grMax = (max - min < 1) ? max + offset : max;  
+  const valRatio = (t - grMin) / (grMax - grMin);
+  return grBottom - Math.floor(valRatio * grHeight);
 }
 
 setInterval(function() {
